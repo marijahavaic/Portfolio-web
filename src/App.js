0d31cefd30ca49e8befefc234a41ebfb85f55d49
Marija Havaic <marija.havaic@gmail.com>
2022-02-21 17:58:06 +0100

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      
+{/*       
       <AboutMe />
       <Skills />
       <Projects />
-      <ContactMe />
+      <ContactMe /> */}
 
     </div>
   );
