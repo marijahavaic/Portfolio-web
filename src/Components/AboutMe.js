d924b653ad7207e9316513c51b58bce82f07e5c0
Marija Havaic <marija.havaic@gmail.com>
2022-02-22 12:58:11 +0100

    Add picture to AboutMe component

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -2,25 +2,37 @@ import React from 'react';
 import './Style/AboutMe.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Container from 'react-bootstrap/esm/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import portfolio_undraw from './Photos/portfolio_undraw.svg'
 
 class AboutMe extends React.Component {
     
     render() { 
         return (
         <div className="AboutMe">
-            <Container bg="dark" variant="dark" >
+            <div className='image-container'>
+                <img className='image' src={portfolio_undraw}/>
+            </div>
+            <div className='text-container'>
+                <h1>I am Marija</h1>
+                <p>
+                    Self-thaught 
+                </p>
+            </div>
+
+
+            {/* <Container>
                 <Row>
                     <Col xs={6} md={4}>
-                    xs=6 md=4
+                        <img className='image' src={portfolio_undraw}/>
                     </Col>
                     <Col xs={12} md={8}>
-                    xs=12 md=8
+                    
                     </Col>
                 </Row>
-            </Container>
+            </Container> */}
         </div>
         )
     }
