b849bfe80dd2a7b50c2605ac3af77bb08d9f41b2
Marija Havaic <marija.havaic@gmail.com>
2022-02-22 12:38:29 +0100

    Style AboutMe component

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './Style/AboutMe.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class AboutMe extends React.Component {
     
     render() { 
         return (
         <div className="AboutMe">
-            
+            <Container bg="dark" variant="dark" >
+                <Row>
+                    <Col xs={6} md={4}>
+                    xs=6 md=4
+                    </Col>
+                    <Col xs={12} md={8}>
+                    xs=12 md=8
+                    </Col>
+                </Row>
+            </Container>
         </div>
         )
     }
