7ad47ae904692539e1c915726965848afe075d25
Marija Havaic <marija.havaic@gmail.com>
2022-02-23 12:00:00 +0100

    Add Project component

---
+++ src/Components/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './Style/Projects.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Projects extends React.Component {
+    
+    render() { 
+        return (
+        <div className="Projects">
+            
+        </div>
+        )
+    }
+}
+
+export default Projects;
