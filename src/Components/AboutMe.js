eb737503397c9bc428841d7f477a114ca8718e30
Marija Havaic <marija.havaic@gmail.com>
2022-02-25 13:08:43 +0100

    Add links to aboutme

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -8,21 +8,34 @@ import Col from 'react-bootstrap/Col';
 
 import portfolio_undraw from './Photos/portfolio_undraw.svg'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGitAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 class AboutMe extends React.Component {
     
     render() { 
         return (
-        <div className="AboutMe">
-             <Container className="justify-content-center align-items-center">
-                <Row xs={1} md={2}>
-                    <Col>
-                        <img className='image' src={portfolio_undraw}/>
+        <div className="AboutMe d-flex justify-content-center align-items-flex-end pt-5 mt-5">
+             <Container className='pt-5 mt-5'>
+                <Row xs={1} md={1} lg={2}>
+                    <Col className='w-50'>
+                        <img className='image' src={portfolio_undraw} className='w-100 h-100'/>
                     </Col>
-                    <Col className="align-self-center">
+                    <Col className="align-self-center w-50">
                         <h1>Hi, I am Marija</h1>
                      <p>
                         A passonate self-taught Front-End Developer. Having an experience of building Web applications with JavaScript and Reactjs and some other cool libraries and frameworks. 
                      </p>
+                        <Container className="justify-content-center align-items-center"  >
+                            <Row xs={2} md={3} lg={5}  >
+                                <Col>
+                                    <FontAwesomeIcon icon={faGitAlt} style={{fontSize: '2.25rem'}}/>
+                                </Col>
+                                <Col>
+                                    <FontAwesomeIcon icon={faLinkedin} style={{fontSize: '2.25rem'}}/>
+                                </Col>
+                            </Row>
+                        </Container> 
                     </Col>
                 </Row>
             </Container>
