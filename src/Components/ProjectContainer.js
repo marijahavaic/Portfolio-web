3115568750643f82e7a4c4c8cddd96c7e4a8c5dd
Marija Havaic <marija.havaic@gmail.com>
2022-06-01 16:40:31 +0200

    Destruct Projects and ProjectContainer
    -make them functional components
    -change padding in Card

--- src/Components/ProjectContainer.js
+++ src/Components/ProjectContainer.js
@@ -10,71 +10,68 @@ import Col from "react-bootstrap/Col";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class ProjectContainer extends React.Component {
-    render() {
-        let { key, image, title, text, badges, icons } = this.props;
-        return (
-            <div>
-                <Col className="d-flex justify-content-center">
-                    <Card
-                        style={{ width: "23rem", height: "30rem" }}
-                        bg="dark"
-                        className="mb-5"
-                        key={key}
-                    >
-                        <Card.Img variant="top" src={image} />
-                        <Card.Body>
-                            <Card.Title>{title}</Card.Title>
-                            <Card.Text
-                                style={{
-                                    height: "6.5rem",
-                                }}
-                            >
-                                {text}
-                            </Card.Text>
-                            <div
-                                className="pt-3 pb-3"
-                                style={{ height: "5.5rem" }}
-                            >
-                                {badges.map((badge, i) => (
-                                    <Badge
-                                        bg="light"
-                                        text="dark"
-                                        className="m-1"
-                                        key={i}
-                                    >
-                                        {" "}
-                                        {badge}
-                                    </Badge>
-                                ))}
-                            </div>
-                            <Container variant="bottom">
-                                <Row
-                                    xs={2}
-                                    md={3}
-                                    lg={4}
-                                    className="d-flex justify-content-center align-items-center"
+const ProjectContainer = ({
+    darkMode,
+    key,
+    image,
+    title,
+    text,
+    badges,
+    icons,
+}) => {
+    return (
+        <div>
+            <Col className="d-flex justify-content-center">
+                <Card
+                    style={{ width: "23rem", height: "32rem" }}
+                    bg={darkMode ? "light" : "dark"}
+                    className="mb-5"
+                    key={key}
+                >
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text
+                            style={{
+                                height: "6.5rem",
+                            }}
+                        >
+                            {text}
+                        </Card.Text>
+                        <div className="pt-3 pb-3" style={{ height: "5.5rem" }}>
+                            {badges.map((badge, i) => (
+                                <Badge
+                                    bg={darkMode ? "dark" : "light"}
+                                    text={darkMode ? "light" : "dark"}
+                                    className="m-1"
+                                    key={i}
                                 >
-                                    {icons.map((icon, i) => (
-                                        <Col>
-                                            <a href={icon.link} key={i}>
-                                                <FontAwesomeIcon
-                                                    icon={icon.name}
-                                                    style={{
-                                                        fontSize: "2.25rem",
-                                                    }}
-                                                />
-                                            </a>
-                                        </Col>
-                                    ))}
-                                </Row>
-                            </Container>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </div>
-        );
-    }
-}
+                                    {" "}
+                                    {badge}
+                                </Badge>
+                            ))}
+                        </div>
+                        <Container variant="bottom">
+                            <Row className="d-flex justify-content-center align-items-center">
+                                {icons.map((icon, i) => (
+                                    <Col>
+                                        <a href={icon.link} key={i}>
+                                            <FontAwesomeIcon
+                                                icon={icon.name}
+                                                style={{
+                                                    fontSize: "2.25rem",
+                                                }}
+                                            />
+                                        </a>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Container>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </div>
+    );
+};
 
 export default ProjectContainer;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,38 +9,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
-class Projects extends React.Component {
-    render() {
-        return (
-            <div className="Projects">
-                <Container
-                    className="d-flex flex-column justify-content-center align-items-center mt-5 pt-5 mb-5 pb-5"
-                    fluid
+const Projects = ({ darkMode }) => {
+    return (
+        <div className="Projects">
+            <Container
+                className="d-flex flex-column justify-content-center align-items-center mt-5 pt-5 mb-5 pb-5"
+                fluid
+            >
+                <h1 className="p-3">
+                    <a id="projects">Projects</a>
+                </h1>
+                <Row
+                    xs={1}
+                    md={2}
+                    lg={3}
+                    className="d-flex justify-content-center align-items-center"
                 >
-                    <h1 className="p-3">
-                        <a id="projects">Projects</a>
-                    </h1>
-                    <Row
-                        xs={1}
-                        md={2}
-                        lg={3}
-                        className="d-flex justify-content-center align-items-center"
-                    >
-                        {projects.map((project, i) => (
-                            <ProjectContainer
-                                key={i}
-                                image={project.image}
-                                title={project.title}
-                                text={project.text}
-                                badges={project.badges}
-                                icons={project.icons}
-                            />
-                        ))}
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
-}
+                    {projects.map((project, i) => (
+                        <ProjectContainer
+                            key={i}
+                            image={project.image}
+                            title={project.title}
+                            text={project.text}
+                            badges={project.badges}
+                            icons={project.icons}
+                            darkMode={darkMode}
+                        />
+                    ))}
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
 export default Projects;
