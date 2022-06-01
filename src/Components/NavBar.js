3f9eb63aec4fbd1321f2a9df1fad6ca5f664bc58
Marija Havaic <marija.havaic@gmail.com>
2022-06-01 15:46:40 +0200

    Refractor NavBar and add darkMode style change

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-class NavBar extends React.Component {
-    
-    render() { 
-        return (
+const NavBar = ({ darkMode }) => {
+    return (
         <div className="NavBar">
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='pt-3'>
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                style={{ fontWeight: 400 }}
+                bg={darkMode ? "light" : "dark"}
+                variant={darkMode ? "light" : "dark"}
+                className="pt-3"
+            >
                 <Container>
-                <Navbar.Brand href="#home" id="home">Home</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="basic-nav-dropdown">
-                <Nav className='me-auto'>
-                    </Nav>                    
-                    <Nav>
-                        <Nav.Link href="#aboutme">About</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#contactme">Contact me</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                
+                    <Navbar.Brand
+                        href="#home"
+                        id="home"
+                        style={{ fontSize: "1.25rem", fontWeight: 600 }}
+                    >
+                        Home
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="basic-nav-dropdown">
+                        <Nav className="me-auto"></Nav>
+                        <Nav>
+                            <Nav.Link href="#aboutme">About</Nav.Link>
+                            <Nav.Link href="#skills">Skills</Nav.Link>
+                            <Nav.Link href="#projects">Projects</Nav.Link>
+                            <Nav.Link href="#contactme">Contact me</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
-        )
-    }
-}
+    );
+};
 
 export default NavBar;
