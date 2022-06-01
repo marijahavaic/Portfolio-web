330adc1d8e9e80a460ae8349c0d913b4a5456725
Marija Havaic <marija.havaic@gmail.com>
2022-06-01 16:42:39 +0200

    Destruct ContactMe

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -5,38 +5,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-class ContactMe extends React.Component {
-    render() {
-        return (
-            <div className="ContactMe d-flex flex-column justify-content-center align-items-center">
-                <Form className="w-75">
-                    <h1 className="text-center">
-                        <a id="contactme">Contact Me</a>
-                    </h1>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlInput1"
-                    >
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="name@example.com"
-                        />
-                    </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1"
-                    >
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        );
-    }
-}
+const ContactMe = () => {
+    return (
+        <div className="ContactMe d-flex flex-column justify-content-center align-items-center">
+            <Form className="w-75">
+                <h1 className="text-center">
+                    <a id="contactme">Contact Me</a>
+                </h1>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                >
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+};
 
 export default ContactMe;
