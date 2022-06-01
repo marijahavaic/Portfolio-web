9c1a9368fbbf41f3ca6b38b38083a4579169806f
Marija Havaic <marija.havaic@gmail.com>
2022-06-01 15:35:18 +0200

    Destruct App.js from class to function Components
    -destruct
    -add darkMode state hook
    -pass darkMode to all components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sass/App.scss";
 
 import NavBar from "./Components/NavBar";
@@ -38,143 +38,121 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
 import Form from "react-bootstrap/Form";
 
-class App extends React.Component {
-    static defaultProps = {
-        projects: [
-            {
-                key: 1,
-                image: lyrics,
-                title: "Lyrics Search",
-                text: `Some quick example text to build on the card title and make up the bulk of the card's content.`,
-                badges: ["HTML5", "CSS3", "Javascript"],
-                icons: [
-                    { name: faGithub, src: "github.com" },
-                    { name: faYoutube, src: "github" },
-                    { name: faGlobe, src: "github" },
-                ],
-            },
-            {
-                key: 2,
-                image: notestodo,
-                title: "Notes Todo App",
-                text: `This is a Notes Todo app in which you can add either a note or a todo, both of which are saved in local storage. This app uses React’s functional components and hooks (useState, useEffect, useContext).`,
-                badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
-                icons: [
-                    {
-                        name: faGithub,
-                        src: "https://github.com/marijahavaic/notes-todo-app",
-                    },
-                    { name: faYoutube, src: "github" },
-                    { name: faGlobe, src: "github" },
-                ],
-            },
-            {
-                key: 3,
-                image: form,
-                title: "Form App",
-                text: `This is a Form app which uses React state hook form handling and validating React form inputs.`,
-                badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
-                icons: [
-                    {
-                        name: faGithub,
-                        src: "https://github.com/marijahavaic/form-app",
-                    },
-                    { name: faYoutube, src: "github" },
-                    { name: faGlobe, src: "github" },
-                ],
-            },
-        ],
-        skills_container: [
-            {
-                name: "Front-End",
-                skills: [
-                    { icon: faHtml5, name: "HTML5" },
-                    { icon: faCss3Alt, name: "CSS3" },
-                    { icon: faJs, name: "JavaScript" },
-                    { icon: faReact, name: "React JS" },
-                    { icon: faRoute, name: "React Router" },
-                    { icon: faBootstrap, name: "Bootstrap5" },
-                    { icon: faFontAwesome, name: "FontAwesome" },
-                    { icon: faClone, name: "MeaterialUI" },
-                ],
-            },
-            {
-                name: "Design",
-                skills: [
-                    { icon: faFigma, name: "Figma" },
-                    { icon: faObjectGroup, name: "Adobe XD" },
-                    { icon: faImage, name: "Photoshop" },
-                    { icon: faImages, name: "Lightroom" },
-                    { icon: faPencilRuler, name: "Illustrator" },
-                ],
-            },
-        ],
+function App() {
+    const projects = [
+        {
+            key: 1,
+            image: lyrics,
+            title: "Lyrics Search",
+            text: `Some quick example text to build on the card title and make up the bulk of the card's content.`,
+            badges: ["HTML5", "CSS3", "Javascript"],
+            icons: [
+                { name: faGithub, src: "github.com" },
+                { name: faYoutube, src: "github" },
+                { name: faGlobe, src: "github" },
+            ],
+        },
+        {
+            key: 2,
+            image: notestodo,
+            title: "Notes Todo App",
+            text: `This is a Notes Todo app in which you can add either a note or a todo, both of which are saved in local storage. This app uses React’s functional components and hooks (useState, useEffect, useContext).`,
+            badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
+            icons: [
+                {
+                    name: faGithub,
+                    src: "https://github.com/marijahavaic/notes-todo-app",
+                },
+                { name: faYoutube, src: "github" },
+                { name: faGlobe, src: "github" },
+            ],
+        },
+        {
+            key: 3,
+            image: form,
+            title: "Form App",
+            text: `This is a Form app which uses React state hook form handling and validating React form inputs.`,
+            badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
+            icons: [
+                {
+                    name: faGithub,
+                    src: "https://github.com/marijahavaic/form-app",
+                },
+                { name: faYoutube, src: "github" },
+                { name: faGlobe, src: "github" },
+            ],
+        },
+    ];
+    const skills_container = [
+        {
+            name: "Front-End",
+            skills: [
+                { icon: faHtml5, name: "HTML5" },
+                { icon: faCss3Alt, name: "CSS3" },
+                { icon: faJs, name: "JavaScript" },
+                { icon: faReact, name: "React JS" },
+                { icon: faRoute, name: "React Router" },
+                { icon: faBootstrap, name: "Bootstrap5" },
+                { icon: faFontAwesome, name: "FontAwesome" },
+                { icon: faClone, name: "MeaterialUI" },
+            ],
+        },
+        {
+            name: "Design",
+            skills: [
+                { icon: faFigma, name: "Figma" },
+                { icon: faObjectGroup, name: "Adobe XD" },
+                { icon: faImage, name: "Photoshop" },
+                { icon: faImages, name: "Lightroom" },
+                { icon: faPencilRuler, name: "Illustrator" },
+            ],
+        },
+    ];
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const handleToggle = () => {
+        setDarkMode(!darkMode);
     };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            ligthMode: true,
-        };
-        this.handleToggle = this.handleToggle.bind(this);
-    }
+    return (
+        <div className={`App ${darkMode ? "Light-mode" : "Dark-mode"}`}>
+            <NavBar darkMode={darkMode} />
+            <Container className="h-100 d-flex justify-content-end">
+                <Form className="position-absolute top-0 end-0 mt-1">
+                    <Form.Switch
+                        onChange={handleToggle}
+                        type="switch"
+                        id="custom-switch"
+                    />
+                </Form>
+            </Container>
 
-    handleToggle() {
-        this.setState({
-            lightMode: !this.state.ligthMode,
-        });
-        console.log(this.state.ligthMode);
-    }
+            <AboutMe />
 
-    render() {
-        return (
-            <div
-                className={`App ${
-                    this.state.lightMode ? "Light-mode" : "Dark-mode"
-                }`}
-            >
-                <NavBar />
-                <Container className="h-100 d-flex justify-content-end">
-                    <Form className="position-absolute top-0 end-0 mt-1">
-                        <Form.Switch
-                            // toggled={this.state.ligthMode}
-                            onChange={this.handleToggle}
-                            type="switch"
-                            id="custom-switch"
+            <Container className="h-100 d-flex justify-content-end pe-5">
+                <a href="#home">
+                    <Button
+                        variant={darkMode ? "dark" : "light"}
+                        className="position-fixed bottom-0 mb-3"
+                    >
+                        <FontAwesomeIcon
+                            icon={faArrowUp}
+                            style={{ fontSize: "1rem" }}
                         />
-                    </Form>
-                </Container>
+                    </Button>
+                </a>
+            </Container>
 
-                <AboutMe />
+            <Skills skills_container={skills_container} darkMode={darkMode} />
 
-                <Container className="h-100 d-flex justify-content-end pe-5">
-                    <a href="#home">
-                        <Button
-                            variant="light"
-                            className="position-fixed bottom-0 mb-3"
-                        >
-                            <FontAwesomeIcon
-                                icon={faArrowUp}
-                                style={{ fontSize: "1rem" }}
-                            />
-                        </Button>
-                    </a>
-                </Container>
+            <Projects projects={projects} darkMode={darkMode} />
 
-                <Skills skills_container={this.props.skills_container} />
+            <ContactMe />
 
-                <Projects projects={this.props.projects} />
-
-                <ContactMe />
-
-                <Footer />
-
-                {/* <div className='position-fixed end-2 right-2'>
-        <a href="#home"><FontAwesomeIcon icon={faArrowUp} style={{fontSize: '1rem'}} /></a>
-        </div> */}
-            </div>
-        );
-    }
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
