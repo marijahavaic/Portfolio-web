da9dcb31b4a6eb8bfdc271a4f5ddb22b7139f2c1
Marija Havaic <marija.havaic@gmail.com>
2022-06-01 16:43:18 +0200

    Destruct Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,22 +1,16 @@
-import React from 'react';
+import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-
-
-
-class Footer extends React.Component {
-    
-    render() { 
-        return (
+const Footer = () => {
+    return (
         <div className="Footer">
-             <Container className='p-3 d-flex text-align-center w-100 justify-content-center align-items-center'>
-                 <h6>Made by Marija HAVAIC</h6>
-             </Container>
+            <Container className="p-3 d-flex text-align-center w-100 justify-content-center align-items-center">
+                <h6>Made by Marija HAVAIC</h6>
+            </Container>
         </div>
-        )
-    }
-}
+    );
+};
 
 export default Footer;
