37d2ffc5742ac69932856015e30ce7d468c4b057
Marija Havaic <marija.havaic@gmail.com>
2022-06-07 18:17:24 +0200

    Add send email feature with EmailJS

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Style/ContactMe.css";
+
+import emailjs from "@emailjs/browser";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 
 const ContactMe = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                form.current,
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+
+        form.current.reset();
+    };
+
     return (
         <div className="ContactMe d-flex flex-column justify-content-center align-items-center">
-            <Form className="w-75">
+            <Form className="w-75" onSubmit={sendEmail} ref={form}>
                 <h1 className="text-center">
                     <a id="contactme">Contact Me</a>
                 </h1>
@@ -16,27 +42,38 @@ const ContactMe = () => {
                     className="mb-3"
                     controlId="exampleForm.ControlInput1"
                 >
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="John Doe"
+                        name="user_name"
+                    />
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control
+                        type="email"
+                        placeholder="name@example.com"
+                        name="user_email"
+                    />
                 </Form.Group>
                 <Form.Group
                     className="mb-3"
                     controlId="exampleForm.ControlTextarea1"
                 >
                     <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} name="message" />
                 </Form.Group>
-                <Button
+                <input
+                    type="submit"
+                    value="Send"
                     style={{
                         backgroundColor: "#f3b700",
                         border: "none",
                         color: "aliceblue",
+                        padding: "0.75rem",
+                        borderRadius: "10%",
                     }}
-                    type="submit"
-                    variant="warning"
-                >
-                    Send Email
-                </Button>
+                    className="icon"
+                />
             </Form>
         </div>
     );
---
+++ src/projectsDetails.js
@@ -0,0 +1,88 @@
+import { faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import {
+    faHtml5,
+    faCss3Alt,
+    faJs,
+    faBootstrap,
+    faFontAwesome,
+    faReact,
+    faFigma,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+    faImage,
+    faImages,
+    faObjectGroup,
+    faPencilRuler,
+    faClone,
+    faRoute,
+} from "@fortawesome/free-solid-svg-icons";
+
+import lyrics from "./Components/Photos/lyrics.png";
+import notestodo from "./Components/Photos/notestodo.png";
+import form from "./Components/Photos/form.png";
+
+export const projects = [
+    {
+        key: 1,
+        image: lyrics,
+        title: "Lyrics Searcher",
+        text: `This project is written with Vanilla JavaScript which uses a third-party API for lyrics searching.`,
+        badges: ["HTML5", "CSS3", "Javascript"],
+        icons: [
+            {
+                name: faGithub,
+                href: "https://github.com/marijahavaic/lyrics-searcher",
+            },
+            // { name: faYoutube, href: "github" },
+            { name: faGlobe, href: "github" },
+        ],
+    },
+    {
+        key: 2,
+        image: notestodo,
+        title: "Notes Todo App",
+        text: `This is a Notes Todo app in which you can add either a note or a todo, both of which are saved in local storage. This app uses React’s functional components and hooks (useState, useEffect, useContext).`,
+        badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
+        icons: [
+            {
+                name: faGithub,
+                href: "https://github.com/marijahavaic/notes-todo-app",
+            },
+            // { name: faYoutube, href: "github" },
+            { name: faGlobe, href: "github" },
+        ],
+    },
+    {
+        key: 3,
+        image: form,
+        title: "Form App",
+        text: `This is a Form app which uses React state hook form handling and validating React form inputs.`,
+        badges: ["HTML5", "CSS3", "Javascript", "React", "FontAwesome"],
+        icons: [
+            {
+                name: faGithub,
+                href: "https://github.com/marijahavaic/form-app",
+            },
+            // { name: faYoutube, href: "github" },
+            { name: faGlobe, href: "github" },
+        ],
+    },
+];
+export const web_skills = [
+    { icon: faHtml5, name: "HTML5" },
+    { icon: faCss3Alt, name: "CSS3" },
+    { icon: faJs, name: "JavaScript" },
+    { icon: faReact, name: "React JS" },
+    { icon: faRoute, name: "React Router" },
+    { icon: faBootstrap, name: "Bootstrap5" },
+    { icon: faFontAwesome, name: "FontAwesome" },
+    { icon: faClone, name: "MeaterialUI" },
+];
+export const design_skills = [
+    { icon: faFigma, name: "Figma" },
+    { icon: faObjectGroup, name: "Adobe XD" },
+    { icon: faImage, name: "Photoshop" },
+    { icon: faImages, name: "Lightroom" },
+    { icon: faPencilRuler, name: "Illustrator" },
+];
