346c086e3e2116c7526f5a73d929bfdc507c7f86
Marija Havaic <marija.havaic@gmail.com>
2022-06-08 17:41:56 +0200

    Translate Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,11 +3,11 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 
-const Footer = () => {
+const Footer = ({ t }) => {
     return (
         <div className="Footer">
             <Container className="p-3 d-flex text-align-center w-100 justify-content-center align-items-center">
-                <h6>Made by Marija HAVAIC</h6>
+                <h6>{t("madeBy")}</h6>
             </Container>
         </div>
     );
